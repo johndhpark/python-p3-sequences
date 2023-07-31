c8b9b618c6233cac57ff90d878ebb5fe980768b1
John DH. Park <john.dh.park@icloud.com>
2023-07-30 20:00:29 -0700

    Completed Sequences assignment

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    result = []
+
+    for idx in range(0, length):
+        if idx == 0 or idx == 1:
+            result.append(idx)
+        else:
+            result.append(result[idx - 1] + result[idx - 2])
+
+    print(result)
+    
